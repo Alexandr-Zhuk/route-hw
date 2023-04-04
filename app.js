5f8edc7221f22c70e382c68f2ec91a5df0b2b177
Alexand-Zhuk <alexandrzhuk88@gmail.com>
2023-04-04 22:22:02 +0300

    update router, add routes for html files, pics, css-files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const http = require('http');
-
+const path = require('path');
 
 const server = http.createServer((req, res) => {
 
@@ -10,8 +10,7 @@ const server = http.createServer((req, res) => {
         {id: 3, title: 'Article #3'},
     ];
 
-    
-    let id = req.url.split('/');
+    let partsUrl = req.url.split('/');
     
     if(req.url === '/'){
         res.end('home page');
@@ -19,21 +18,59 @@ const server = http.createServer((req, res) => {
         res.end('Category page');
     } else if(req.url === '/category/product'){
         res.end('Product page');
-    } else if(req.url === '/picture/cat.jpg'){
-        let pic = fs.readFileSync('./images/cat.jpg');
-        res.end(pic);
-    } else if(req.url === `/article/${id[2]}`){
-        let findArticle = false;
-        for(let i = 0; i < articles.length; i++){
-            if(articles[i].id === Number(id[2])){
-                res.end(articles[i].title);
-                findArticle = true;
+    } else if(req.url === `/${partsUrl[1]}/${partsUrl[2]}`){
+        let ext = path.extname(`/${partsUrl[1]}/${partsUrl[2]}`);
+        let dir = '';
+
+        const render = () => {
+            fs.access(`./${dir}${partsUrl[2]}`, fs.constants.F_OK, (err) => {
+                if (err)
+                    console.log('No file')
+                else {
+                    let data = fs.readFileSync(`./${dir}${partsUrl[2]}`);
+                    res.end(data);
+                }
+            });
+        };
+
+        if(ext === '.css'){
+            dir = 'styles/';
+            render();
+        } else if(ext === '.jpg' || ext === '.png' || ext === '.jpeg'){
+            dir = 'images/';
+            render();
+        } else if(ext === ''){
+            if(partsUrl[1] === 'article'){
+                let findArticle = false;
+                for(let i = 0; i < articles.length; i++){
+                    if(articles[i].id === Number(partsUrl[2])){
+                        res.end(articles[i].title);
+                        findArticle = true;
+                    }
+                }
+                if(!findArticle){
+                    res.end('404 page');
+                }
+                res.end();
             }
-        }
-        if(!findArticle){
+        } else{
             res.end('404 page');
         }
-        res.end();
+    }  else if(req.url === `/${partsUrl[1]}`){
+        let ext = path.extname(`/${partsUrl[1]}`);
+        
+        if(ext === '.html'){
+            fs.access(`./${partsUrl[1]}`, fs.constants.F_OK, (err) => {
+                if (err)
+                    res.end('404 page');
+                else {
+                    let data = fs.readFileSync(`./${partsUrl[1]}`);
+                    res.end(data);
+                }
+            });
+        }else{
+            res.end('404 page');
+        }
         
     } else{
         res.end('404 page');
